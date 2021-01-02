dc7afbb9e92081a9a1b027e83b355c9f9bc651aa
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-02 20:36:37 +0530

    initialised app with basic components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-light shadow-sm mb-3" style={{backgroundColor: "#e3f2fd"}}>
+      <div class="container-fluid">
+      <a class="navbar-brand" href="/">Clumsy Crypto</a>
+    </div>
+    </nav>
+  );
+};
+
+export default Navbar;
