03b177e9696233240ec0a2a33acba8fc45e920a0
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-03 16:12:26 +0530

    moved navbar component to App
    added currency and symbol values to state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
 import Today from "./Components/Today";
 import History from "./Components/History";
 
 const App = () => {
+  const [currency, setCurrency] = useState("USD");
+  const [symbol, setSymbol] = useState("$");
+
   return (
     <div className="App">
-      <Navbar />
-      <Today />
-      <History />
+      <nav
+        className="navbar navbar-light shadow-sm mb-3"
+        style={{ backgroundColor: "#e3f2fd" }}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Clumsy Crypto
+          </a>
+        </div>
+      </nav>
+      <div className="container px-4">
+        <Today currency={currency} symbol={symbol} />
+        <History currency={currency} symbol={symbol} />
+      </div>
     </div>
   );
 };
