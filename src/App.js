bb4a7a4773c2ccfed9096092fa8cebe45907e9f8
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-03 19:30:30 +0530

    added currency option
    switching currency changes symbol

--- src/App.js
+++ src/App.js
@@ -4,18 +4,51 @@ import History from "./Components/History";
 
 const App = () => {
   const [currency, setCurrency] = useState("USD");
-  const [symbol, setSymbol] = useState("$");
-
+  const [symbol, setSymbol] = useState(36);
+  const symbols = { USD: 36, INR: 8377 };
   return (
     <div className="App pb-5">
       <nav
-        className="navbar navbar-light shadow-sm mb-3"
+        className="navbar navbar-light shadow-sm mb-3 navbar-expand-sm "
         style={{ backgroundColor: "#e3f2fd" }}
       >
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             Clumsy Crypto
           </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item d-flex align-items-center">
+                <div className="me-2">Currency:</div>
+                <select
+                  className="form-select"
+                  value={currency}
+                  onChange={(e) => {
+                    setCurrency(e.target.value);
+                    setSymbol(symbols[e.target.value]);
+                  }}
+                >
+                  <option value="USD" title="United States Dollars">
+                    USD
+                  </option>
+                  <option value="INR" title="Indian Rupees">
+                    INR
+                  </option>
+                </select>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
       <div className="container px-4">
